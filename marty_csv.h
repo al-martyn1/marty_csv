1c7d80e26e8ceb23c9b45265f0cb58f239c2211e
al-martyn1 <amart@mail.ru>
2023-08-19 02:47:22 +0300

    Запупырил хелперы для CSV в отдельную репу

---
+++ marty_csv.h
@@ -0,0 +1,272 @@
+/* \file
+   \brief Утилзы для marty_csv
+
+ */
+
+#pragma once
+
+#include "utils.h"
+
+
+
+//----------------------------------------------------------------------------
+namespace marty_csv {
+
+
+// https://ru.wikipedia.org/wiki/CSV
+// https://datatracker.ietf.org/doc/html/rfc4180
+inline
+std::string serializeToCsvField(const std::string &s, char sep=';')
+{
+    bool needQuoting = false;
+    for(auto ch: s)
+    {
+        if (ch=='\"' || ch==sep)
+        {
+            needQuoting = true;
+        }
+    }
+
+    if (!needQuoting)
+    {
+        return s;
+    }
+
+    std::string resStr;
+    resStr.append(1, '\"');
+    for(auto ch: s)
+    {
+        if (ch=='\"')
+        {
+            resStr.append(1, '\"');
+        }
+
+        resStr.append(1, ch);
+    }
+    resStr.append(1, '\"');
+
+    return resStr;
+
+}
+
+
+inline
+std::string serializeToCsv(const std::vector< std::vector<std::string> > &csvLines, const std::string &lf="\n", char sep=';')
+{
+    std::string resStr; resStr.reserve(csvLines.size()*48);
+
+    for(const auto &line: csvLines)
+    {
+        std::string lineStr;
+        for(const auto &field: line)
+        {
+            if (!lineStr.empty())
+            {
+                lineStr.append(1, sep);
+            }
+
+            lineStr.append(serializeToCsvField(field, sep));
+        }
+
+        if (lineStr.empty())
+        {
+            continue;
+        }
+
+        resStr.append(lineStr);
+        resStr.append(lf);
+    }
+
+    return resStr;
+}
+
+
+
+
+
+//! CR всегда игнорируем 
+inline
+std::vector< std::vector<std::string> > deserializeFieldsFromCsvLines(const std::string &str, char sep=';')
+{
+    std::string                              curField;
+    std::vector<std::string>                 curLine;
+    std::vector< std::vector<std::string> >  resVec ;
+
+
+    enum State
+    {
+        startReading, // start reading field
+        readNormal ,
+        readQuoted ,
+        readQuotedWaitSecondQuot
+    };
+
+    State state = startReading;
+
+    auto append = [&](char ch)
+    {
+        curField.append(1,ch);
+    };
+
+    auto addCurField = [&]()
+    {
+        curLine.emplace_back(curField);
+        curField.clear();
+        state = startReading;
+    };
+
+    auto addCurLine = [&]()
+    {
+        if (curField.empty() && curLine.empty())
+        {
+            // Это просто пустая строка, не надо ничего добавлять
+            return;
+        }
+
+        addCurField(); // finalize field
+        resVec.emplace_back(curLine);
+        curLine.clear();
+    };
+
+
+    for(char ch : str)
+    {
+        if (state==startReading) // Только начали читать поле
+        {
+            if (ch=='\"')
+            {
+                state = readQuoted; // Меняем состояние на "чтение закавыченного"
+                // append(ch); // Саму кавычку пропускаем
+            }
+            else if (ch==sep) // Нашли разделитель
+            {
+                // append(ch); // Разделитель игнорируем
+                addCurField();
+            }
+            else if (ch=='\r') // Символ CR - полный игнор
+            {
+            }
+            else if (ch=='\n') // Символ LF - завершение строки
+            {
+                addCurLine();
+            }
+            else // Все прочие символы просто добавляем
+            {
+                append(ch);
+            }
+        }
+        else if (state==readNormal) // Читаем поле без кавычек
+        {
+            if (ch==sep) // Нашли разделитель
+            {
+                // append(ch); // Разделитель игнорируем
+                addCurField();
+            }
+            else if (ch=='\r') // Символ CR - полный игнор
+            {
+            }
+            else if (ch=='\n') // Символ LF - завершение строки
+            {
+                addCurLine();
+            }
+            else // Все прочие символы просто добавляем
+            {
+                append(ch);
+            }
+        }
+        else if (state==readQuoted) // Читаем поле в кавычках
+        {
+            if (ch=='\"')
+            {
+                state = readQuotedWaitSecondQuot; // Меняем состояние на "ожидание дубля кавычки"
+                // append(ch); // Саму кавычку пропускаем
+            }
+            // Разделитель игнорируем как пазделитель, он внутри закавыченного поля - как простой символ
+            else if (ch=='\r') // Символ CR - полный игнор
+            {
+            }
+            else if (ch=='\n') // Символ LF - завершение строки, но внутри закавыченного поля это означает перенос строки внутри поля
+            {                  // Просто добавляем как все прочие символы в текущее поле
+                append(ch);
+            }
+            else // Все прочие символы просто добавляем
+            {
+                append(ch);
+            }
+        }
+        else if (state==readQuotedWaitSecondQuot) // Читаем поле в кавычках, ожидаем кавычку или разделитель полей
+        {
+            if (ch=='\"')
+            {
+                state = readQuoted; // Меняем состояние на "чтение закавыченного"
+                append(ch); // Кавычку добавляем
+            }
+            else if (ch==sep) // Нашли разделитель
+            {
+                // append(ch); // Разделитель игнорируем
+                addCurField();
+            }
+            else // Все прочие символы просто добавляем
+            {
+                append('\"'); // одиночная кавычка внутри закавыченного поля - работает как одиночная кавычка
+                append(ch);
+                state = readQuoted;
+            }
+
+        }
+        else
+        {
+            // По идее такого не должно, тут наверное надо что-то кинуть
+        }
+    
+    } // for(char ch : str)
+
+
+    addCurLine(); // Добавляем, если не пусто
+
+
+    return resVec;
+
+}
+
+template<typename ToWideConverter> inline
+std::vector< std::vector<std::wstring> > csvFieldsToWide(const std::vector< std::vector<std::string> > &lines, ToWideConverter converter)
+{
+    std::vector< std::vector<std::wstring> > recVec; recVec.reserve(lines.size());
+
+    for(const auto &line: lines)
+    {
+        std::vector<std::wstring> wl; wl.reserve(line.size());
+        for(const auto &field: line)
+        {
+            wl.emplace_back(converter(field));
+        }
+
+        recVec.emplace_back(wl);
+    }
+
+    return recVec;
+}
+
+template<typename ToAsciiConverter> inline
+std::vector< std::vector<std::string> > csvFieldsToAnsi(const std::vector< std::vector<std::wstring> > &lines, ToAsciiConverter converter)
+{
+    std::vector< std::vector<std::string> > recVec; recVec.reserve(lines.size());
+
+    for(const auto &line: lines)
+    {
+        std::vector<std::string> wl; wl.reserve(line.size());
+        for(const auto &field: line)
+        {
+            wl.emplace_back(converter(field));
+        }
+
+        recVec.emplace_back(wl);
+    }
+
+    return recVec;
+}
+
+
+} // namespace marty_csv
+
---
+++ utils.h
@@ -0,0 +1,192 @@
+/* \file
+   \brief Утилзы для marty_csv
+
+ */
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+
+
+//----------------------------------------------------------------------------
+
+
+
+//----------------------------------------------------------------------------
+namespace marty_csv {
+
+namespace utils {
+
+
+
+//----------------------------------------------------------------------------
+template<typename ActualPred, typename ParamType>
+struct not_pred
+{
+    const ActualPred &actualPred; //!< Ссылка на предикат для отрицания его результата
+    //! Конструктор предиката отрицания
+    not_pred( const ActualPred &a /*!< предикат для отрицания */ ) : actualPred(a) {}
+    //! Оператор функционального объекта
+    bool operator()( ParamType ch ) const { return !actualPred(ch); }
+
+    // not_pred(const not_pred&) = delete;
+    // not_pred(not_pred&&) = delete;
+    // not_pred& operator=(const not_pred&) = delete;
+    // not_pred& operator=(not_pred&&) = delete;
+
+};
+
+//----------------------------------------------------------------------------
+template <typename StringType> inline bool starts_with( const StringType &str, const StringType &prefix )
+{
+    if (str.size()<prefix.size())
+        return false;
+
+    return str.compare( 0, prefix.size(), prefix )==0;
+}
+
+//----------------------------------------------------------------------------
+template <typename StringType, typename TrimPred> inline void ltrim( StringType &s, const TrimPred &pred )
+{
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), not_pred<TrimPred, typename StringType::value_type>(pred) ));
+}
+
+//-----------------------------------------------------------------------------
+template <typename StringType, typename TrimPred > inline void rtrim(StringType &s, const TrimPred &pred)
+{
+    s.erase( std::find_if(s.rbegin(), s.rend(), not_pred<TrimPred,typename StringType::value_type>(pred) ).base(), s.end());
+}
+
+//-----------------------------------------------------------------------------
+template <typename StringType, typename TrimPred> inline void trim(StringType &s, const TrimPred &pred)
+{
+    ltrim(s,pred); rtrim(s,pred);
+}
+
+//----------------------------------------------------------------------------
+
+
+
+
+//----------------------------------------------------------------------------
+inline
+std::string to_ascii(const char* str)
+{
+    return str ? std::string(str) : std::string();
+}
+
+//----------------------------------------------------------------------------
+inline
+std::string to_ascii(const std::string &str)
+{
+    return str;
+}
+
+//----------------------------------------------------------------------------
+inline
+std::string to_ascii(const std::wstring &str)
+{
+    std::string strRes; strRes.reserve(str.size());
+    for(auto ch : str)
+        strRes.append(1, (char)(unsigned char)ch);
+    return strRes;
+}
+
+//----------------------------------------------------------------------------
+inline
+std::string to_ascii(const wchar_t* str)
+{
+    return str ? to_ascii(std::wstring(str)) : std::string();
+}
+
+//----------------------------------------------------------------------------
+
+
+
+//----------------------------------------------------------------------------
+inline
+std::wstring to_wide(const wchar_t* str)
+{
+    return str ? std::wstring(str) : std::wstring();
+}
+
+//----------------------------------------------------------------------------
+inline
+std::wstring to_wide(const std::wstring &str)
+{
+    return str;
+}
+
+//----------------------------------------------------------------------------
+inline
+std::wstring to_wide(const std::string &str)
+{
+    std::wstring strRes; strRes.reserve(str.size());
+    for(auto ch : str)
+        strRes.append(1, (wchar_t)ch);
+    return strRes;
+}
+
+//----------------------------------------------------------------------------
+inline
+std::wstring to_wide(const char* str)
+{
+    return str ? to_wide(std::string(str)) : std::wstring();
+}
+
+//----------------------------------------------------------------------------
+
+
+
+//----------------------------------------------------------------------------
+
+inline bool    is_lower( char ch )     { return (ch>='a' && ch<='z'); }
+inline bool    is_upper( char ch )     { return (ch>='A' && ch<='Z'); }
+
+inline bool    is_lower( wchar_t ch )  { return (ch>=L'a' && ch<=L'z'); }
+inline bool    is_upper( wchar_t ch )  { return (ch>=L'A' && ch<=L'Z'); }
+
+inline char    to_lower( char ch )     { return is_upper(ch) ? ch-'A'+'a' : ch; }
+inline char    to_upper( char ch )     { return is_lower(ch) ? ch-'a'+'A' : ch; }
+
+inline wchar_t to_lower( wchar_t ch )  { return (wchar_t)(is_upper(ch) ? ch-L'A'+L'a' : ch); }
+inline wchar_t to_upper( wchar_t ch )  { return (wchar_t)(is_lower(ch) ? ch-L'a'+L'A' : ch); }
+
+template< class CharT, class Traits = std::char_traits<CharT>, class Allocator = std::allocator<CharT> >
+inline std::basic_string< CharT, Traits, Allocator >
+to_lower( const std::basic_string< CharT, Traits, Allocator > &str )
+{
+    std::basic_string< CharT, Traits, Allocator > resStr; resStr.reserve(str.size());
+    for( auto it = str.begin(); it != str.end(); ++it )
+    {
+        resStr.append( 1, to_lower(*it) );
+    }
+
+    return resStr;
+}
+
+template< class CharT, class Traits = std::char_traits<CharT>, class Allocator = std::allocator<CharT> >
+inline std::basic_string< CharT, Traits, Allocator >
+to_upper( const std::basic_string< CharT, Traits, Allocator > &str )
+{
+    std::basic_string< CharT, Traits, Allocator > resStr; resStr.reserve(str.size());
+    for( auto it = str.begin(); it != str.end(); ++it )
+    {
+        resStr.append( 1, to_upper(*it) );
+    }
+
+    return resStr;
+}
+
+//----------------------------------------------------------------------------
+
+
+
+//----------------------------------------------------------------------------
+
+} // namespace utils
+
+} // namespace marty_csv
+
