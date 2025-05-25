2d1e964c2d04d722371b95d6fd86717328107f6b
al-martyn1 <amart@mail.ru>
2025-05-26 00:06:03 +0300

    Начал присовывать новую версию

---
+++ marty_csv_new.h
@@ -0,0 +1,16 @@
+/* \file
+   \brief marty_csv_new - новое API для работы с CSV - marty::csv
+
+ */
+
+#pragma once
+
+namespace marty {
+namespace csv {
+
+
+
+} // namespace csv
+} // namespace marty
+
+
